ffcefc88b1116ca14286e8f7c1434f4596e35c07
Andy CAO <andyi@live.cn>
2018-08-13 10:49:08 +0800

    begin

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+	service "./service"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+	}
+	server := service.NewServer()
+	server.Run(":" + port)
+}
